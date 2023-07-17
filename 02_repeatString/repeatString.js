c760ab379e2f57e5981cb46b277878ec8bed09ef
shrestharukesh1213 <125758326+shrestharukesh1213@users.noreply.github.com>
2023-07-17 17:04:11 +0545

    Completed exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (str, repeat) {
+  let str1 = "";
+  if (repeat < 0) {
+    return "ERROR";
+  } else {
+    for (let i = 0; i < repeat; i++) {
+      str1 = str1 + str;
+    }
+    return str1;
+  }
 };
 
 // Do not edit below this line
