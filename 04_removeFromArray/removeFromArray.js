b115e7238a90f5f0bf763b2a40a66bedf2f053e4
shrestharukesh1213 <125758326+shrestharukesh1213@users.noreply.github.com>
2023-07-17 18:53:09 +0545

    Completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...removeElement) {
+  for (j = 0; j <= removeElement.length - 1; j++) {
+    for (let i = 0; i <= arr.length; i++) {
+      if (removeElement[j] == arr[i]) {
+        const index = arr.indexOf(removeElement[j]);
+        arr.splice(index, 1);
+      }
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
