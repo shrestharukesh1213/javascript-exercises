146c28202658791c073bbf713b89e0f2ba7e0139
shrestharukesh1213 <125758326+shrestharukesh1213@users.noreply.github.com>
2023-07-17 19:19:02 +0545

    Completed exercise 6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function (firstIndex, lastIndex) {
+  let sum = 0;
+  if (
+    Number.isInteger(firstIndex) &&
+    Number.isInteger(lastIndex) &&
+    lastIndex > 0 &&
+    firstIndex > 0
+  ) {
+    if (firstIndex < lastIndex) {
+      for (i = firstIndex; i <= lastIndex; i++) {
+        sum += i;
+      }
+    } else if (firstIndex > lastIndex) {
+      for (i = firstIndex; i >= lastIndex; i--) {
+        sum += i;
+      }
+    }
+    return sum;
+  } else return "ERROR";
 };
 
 // Do not edit below this line
